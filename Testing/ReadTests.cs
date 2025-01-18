361719874f8ac178f505a9c47f8b4e7f94ccb532
grovre <landonvee111@gmail.com>
2025-01-18 02:55:50 -0600

    Rewrite byte tests for streamlined typing

--- Testing/ReadTests.cs
+++ Testing/ReadTests.cs
@@ -12,34 +12,59 @@ namespace Testing;
 [TestFixture]
 internal class ReadTests
 {
+    private unsafe void ReadTest<T>(T src, ref T dst) where T : unmanaged
+    {
+        var defensiveSrcCopy = src;
+        var pid = Environment.ProcessId;
+        using var mem = new Memlib.Memory(pid);
+        mem.Open();
+
+        var pSrc = new nint(&src);
+        dst = mem.Read<T>(pSrc);
+
+        Assert.That(dst, Is.EqualTo(defensiveSrcCopy));
+        Assert.That(dst, Is.EqualTo(src));
+    }
+
+    private unsafe void WriteTest<T>(T src, ref T dst) where T : unmanaged
+    {
+        var defensiveSrcCopy = src;
+        var pid = Environment.ProcessId;
+        using var mem = new Memlib.Memory(pid);
+        mem.Open();
+
+        fixed (T* pDst = &dst)
+        {
+            mem.Write(new nint(pDst), src);
+        }
+
+        Assert.That(dst, Is.EqualTo(defensiveSrcCopy));
+        Assert.That(dst, Is.EqualTo(src));
+    }
+
     [Test]
     public unsafe void ByteReadTest()
     {
-        var length = sizeof(Int128);
-        Span<byte> bx = stackalloc byte[length];
+        const int length = 0x40;
+        Span<byte> bSrc = stackalloc byte[length];
+        Span<byte> bDst = stackalloc byte[length];
+
+        Random.Shared.NextBytes(bSrc);
 
         var pid = Environment.ProcessId;
         using var mem = new Memlib.Memory(pid);
         mem.Open();
 
         for (var i = 0; i < length; i++)
-            bx[i] = (byte)i;
-
-        fixed (byte* pbx = bx)
         {
-            for (var offset = 0; offset < length; offset++)
-            {
-                var addr = new nint(pbx + offset);
-                var b = mem.Read<byte>(addr);
-                Assert.That(b, Is.EqualTo(bx[offset]));
-            }
+            ReadTest(bSrc[i], ref bDst[i]);
         }
     }
 
     [Test]
     public unsafe void ByteWriteTest()
     {
-        var length = sizeof(Int128);
+        var length = 0x40;
         Span<byte> bSrc = stackalloc byte[length];
         Span<byte> bDst = stackalloc byte[length];
 
@@ -48,18 +73,8 @@ internal class ReadTests
         mem.Open();
 
         for (var i = 0; i < length; i++)
-            bSrc[i] = (byte)i;
-
-        fixed (byte* pbDst = bSrc)
         {
-            for (var offset = 0; offset < length; offset++)
-            {
-                var addr = new nint(pbDst + offset);
-                mem.Write(addr, bSrc[offset]);
-            }
+            WriteTest(bSrc[i], ref bDst[i]);
         }
-
-        for (var i = 0; i < length; i++)
-            Assert.That(bDst[i], Is.EqualTo(bSrc[i]));
     }
 }
