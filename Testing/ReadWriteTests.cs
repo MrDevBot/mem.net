a9f4bb15aa46a567da2760d2fafc3b60e5050c4f
grovre <landonvee111@gmail.com>
2025-01-18 12:54:16 -0600

    Shorten tests, make more readable

--- Testing/ReadWriteTests.cs
+++ Testing/ReadWriteTests.cs
@@ -13,7 +13,9 @@ namespace Testing;
 [TestFixture]
 internal class ReadWriteTests
 {
-    private unsafe void ReadTest<T>(T src, ref T dst) where T : unmanaged
+    const int Length = 0x40;
+
+    private static unsafe void ReadTest<T>(T src, ref T dst) where T : unmanaged
     {
         var defensiveSrcCopy = src;
         var pid = Environment.ProcessId;
@@ -23,11 +25,11 @@ internal class ReadWriteTests
         var pSrc = new nint(&src);
         dst = mem.Read<T>(pSrc);
 
-        Assert.That(dst, Is.EqualTo(defensiveSrcCopy));
+        Assert.That(src, Is.EqualTo(defensiveSrcCopy));
         Assert.That(dst, Is.EqualTo(src));
     }
 
-    private unsafe void WriteTest<T>(T src, ref T dst) where T : unmanaged
+    private static unsafe void WriteTest<T>(T src, ref T dst) where T : unmanaged
     {
         var defensiveSrcCopy = src;
         var pid = Environment.ProcessId;
@@ -39,18 +41,16 @@ internal class ReadWriteTests
             mem.Write(new nint(pDst), src);
         }
 
-        Assert.That(dst, Is.EqualTo(defensiveSrcCopy));
+        Assert.That(src, Is.EqualTo(defensiveSrcCopy));
         Assert.That(dst, Is.EqualTo(src));
     }
 
-    [Test]
-    public unsafe void ByteReadTest()
+    private static unsafe void PerformReadWriteTest<T>(int length, Random rand) where T : unmanaged
     {
-        const int length = 0x40;
-        Span<byte> bSrc = stackalloc byte[length];
-        Span<byte> bDst = stackalloc byte[length];
+        Span<T> src = stackalloc T[length];
+        Span<T> dst = stackalloc T[length];
 
-        Random.Shared.NextBytes(bSrc);
+        rand.NextBytes(MemoryMarshal.AsBytes(src));
 
         var pid = Environment.ProcessId;
         using var mem = new Memlib.Memory(pid);
@@ -58,296 +58,39 @@ internal class ReadWriteTests
 
         for (var i = 0; i < length; i++)
         {
-            ReadTest(bSrc[i], ref bDst[i]);
+            ReadTest(src[i], ref dst[i]);
+        }
+
+        dst.Clear();
+        rand.NextBytes(MemoryMarshal.AsBytes(src));
+
+        for (var i = 0; i < length; i++)
+        {
+            WriteTest(src[i], ref dst[i]);
         }
     }
 
     [Test]
-    public unsafe void ByteWriteTest()
+    public unsafe void BasicReadWriteTest()
     {
-        var length = 0x40;
-        Span<byte> bSrc = stackalloc byte[length];
-        Span<byte> bDst = stackalloc byte[length];
-
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(bSrc[i], ref bDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void ShortReadTest()
-    {
-        const int length = 0x40;
-        Span<short> sSrc = stackalloc short[length];
-        Span<short> sDst = stackalloc short[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(sSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(sSrc[i], ref sDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void ShortWriteTest()
-    {
-        const int length = 0x40;
-        Span<short> sSrc = stackalloc short[length];
-        Span<short> sDst = stackalloc short[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(sSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(sSrc[i], ref sDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void IntReadTest()
-    {
-        const int length = 0x40;
-        Span<int> iSrc = stackalloc int[length];
-        Span<int> iDst = stackalloc int[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(iSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(iSrc[i], ref iDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void LongReadTest()
-    {
-        const int length = 0x40;
-        Span<long> lSrc = stackalloc long[length];
-        Span<long> lDst = stackalloc long[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(lSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(lSrc[i], ref lDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void LongWriteTest()
-    {
-        const int length = 0x40;
-        Span<long> lSrc = stackalloc long[length];
-        Span<long> lDst = stackalloc long[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(lSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(lSrc[i], ref lDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void Int128ReadTest()
-    {
-        const int length = 0x40;
-        Span<Int128> i128Src = stackalloc Int128[length];
-        Span<Int128> i128Dst = stackalloc Int128[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(i128Src));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(i128Src[i], ref i128Dst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void Int128WriteTest()
-    {
-        const int length = 0x40;
-        Span<Int128> i128Src = stackalloc Int128[length];
-        Span<Int128> i128Dst = stackalloc Int128[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(i128Src));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(i128Src[i], ref i128Dst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void FloatReadTest()
-    {
-        const int length = 0x40;
-        Span<float> fSrc = stackalloc float[length];
-        Span<float> fDst = stackalloc float[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(fSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(fSrc[i], ref fDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void FloatWriteTest()
-    {
-        const int length = 0x40;
-        Span<float> fSrc = stackalloc float[length];
-        Span<float> fDst = stackalloc float[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(fSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(fSrc[i], ref fDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void DoubleReadTest()
-    {
-        const int length = 0x40;
-        Span<double> dSrc = stackalloc double[length];
-        Span<double> dDst = stackalloc double[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(dSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(dSrc[i], ref dDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void DoubleWriteTest()
-    {
-        const int length = 0x40;
-        Span<double> dSrc = stackalloc double[length];
-        Span<double> dDst = stackalloc double[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(dSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(dSrc[i], ref dDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void DecimalReadTest()
-    {
-        const int length = 0x40;
-        Span<decimal> decSrc = stackalloc decimal[length];
-        Span<decimal> decDst = stackalloc decimal[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(decSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(decSrc[i], ref decDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void DecimalWriteTest() 
-    {
-        var length = 0x40;
-        Span<decimal> decSrc = stackalloc decimal[length];
-        Span<decimal> decDst = stackalloc decimal[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(decSrc));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(decSrc[i], ref decDst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void Vector3ReadTest()
-    {
-        const int length = 0x40;
-        Span<Vector3> v3Src = stackalloc Vector3[length];
-        Span<Vector3> v3Dst = stackalloc Vector3[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(v3Src));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(v3Src[i], ref v3Dst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void Vector3WriteTest()
-    {
-        const int length = 0x40;
-        Span<Vector3> v3Src = stackalloc Vector3[length];
-        Span<Vector3> v3Dst = stackalloc Vector3[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(v3Src));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(v3Src[i], ref v3Dst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void ManagedStructReadTest()
-    {
-        const int length = 0x40;
-        Span<Matrix4x4> m4Src = stackalloc Matrix4x4[length];
-        Span<Matrix4x4> m4Dst = stackalloc Matrix4x4[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(m4Src));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            ReadTest(m4Src[i], ref m4Dst[i]);
-        }
-    }
-
-    [Test]
-    public unsafe void ManagedStructWriteTest()
-    {
-        const int length = 0x40;
-        Span<Matrix4x4> m4Src = stackalloc Matrix4x4[length];
-        Span<Matrix4x4> m4Dst = stackalloc Matrix4x4[length];
-        Random.Shared.NextBytes(MemoryMarshal.AsBytes(m4Src));
-        var pid = Environment.ProcessId;
-        using var mem = new Memlib.Memory(pid);
-        mem.Open();
-        for (var i = 0; i < length; i++)
-        {
-            WriteTest(m4Src[i], ref m4Dst[i]);
-        }
+        PerformReadWriteTest<byte>(Length, Random.Shared);
+        PerformReadWriteTest<sbyte>(Length, Random.Shared);
+        PerformReadWriteTest<short>(Length, Random.Shared);
+        PerformReadWriteTest<ushort>(Length, Random.Shared);
+        PerformReadWriteTest<int>(Length, Random.Shared);
+        PerformReadWriteTest<uint>(Length, Random.Shared);
+        PerformReadWriteTest<long>(Length, Random.Shared);
+        PerformReadWriteTest<ulong>(Length, Random.Shared);
+        PerformReadWriteTest<Int128>(Length, Random.Shared);
+        PerformReadWriteTest<UInt128>(Length, Random.Shared);
+        PerformReadWriteTest<Half>(Length, Random.Shared);
+        PerformReadWriteTest<float>(Length, Random.Shared);
+        PerformReadWriteTest<double>(Length, Random.Shared);
+        PerformReadWriteTest<decimal>(Length, Random.Shared);
+        PerformReadWriteTest<Vector2>(Length, Random.Shared);
+        PerformReadWriteTest<Vector3>(Length, Random.Shared);
+        PerformReadWriteTest<Vector4>(Length, Random.Shared);
+        PerformReadWriteTest<Matrix4x4>(Length, Random.Shared);
+        PerformReadWriteTest<Quaternion>(Length, Random.Shared);
     }
 }
