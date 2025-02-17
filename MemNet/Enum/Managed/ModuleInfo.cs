d4c95d8b02871ea97aee8d3710a4911102eee605
MrDevBot <alexeldera04@gmail.com>
2025-02-17 15:37:14 +1000

    Externalise `ModuleInfo`

---
+++ MemNet/Enum/Managed/ModuleInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace MemNet.Enum.Managed;
+
+/// <summary>
+/// Contains information about a module loaded in the target process.
+/// A managed representation of the MODULEINFO structure with additional data.
+/// </summary>
+public struct ModuleInfo
+{
+    public string ModuleName;
+    public IntPtr Base;
+    public IntPtr EntryPoint;
+    public uint SizeOfImage;
+}
