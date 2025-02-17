f5311bede41fdd5bba8ca9f47ca6fc594dd87660
MrDevBot <alexeldera04@gmail.com>
2025-02-17 15:37:25 +1000

    Externalise `MemoryPageInformation`

---
+++ MemNet/Enum/Managed/MemoryPageInformation.cs
@@ -0,0 +1,16 @@
+﻿namespace MemNet.Enum.Managed;
+
+/// <summary>
+/// Contains information about a memory page in the target process.
+/// A managed representation of the MEMORY_BASIC_INFORMATION structure.
+/// </summary>
+public struct MemoryPageInformation
+{
+    public IntPtr BaseAddress;
+    public IntPtr AllocationBase;
+    public MemoryProtection AllocationProtect;
+    public ulong RegionSize;
+    public MemoryState State;
+    public MemoryProtection Protect;
+    public MemoryType Type;
+}
